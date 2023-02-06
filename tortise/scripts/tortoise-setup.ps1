d8dfa2a06b397125d0fa005310057d697ca68861
KD <kdoobay@knights.ucf.edu>
2023-02-06 16:28:02 -0700

    Update tortoise-setup.ps1
    Add comment to tortoise-setup for pytorch install command

--- tortise/scripts/tortoise-setup.ps1
+++ tortise/scripts/tortoise-setup.ps1
@@ -2,6 +2,7 @@
 $RepoName = 'tortoise-tts-fast'
 $RepoOwner = '152334H'
 $RepoUrl = 'https://github.com/'+$RepoOwner+'/'+$RepoName+'.git'
+# Generally speaking, having these commands hardcoded is bad practice. You can source these commands from here: https://pytorch.org/get-started/locally/
 $PytorchConda = 'conda install pytorch torchvision torchaudio pytorch-cuda=11.6 -c pytorch -c nvidia'
 $PytorchPip = 'pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116'
 
